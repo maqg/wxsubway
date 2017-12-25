82d73d9eaa4fc77d46c57c3aa3fd2a8823545717
maqg <maquangang@gmail.com>
2017-12-25 16:04:22 +0800

    for pages move

--- client/pages/config/config.js
+++ client/pages/config/config.js
@@ -9,9 +9,16 @@ Page({
     dataUpdateTime: "2017年8月10日",
     desc: "超级简单实用的北京地铁票价助手，采用全离线数据，可以查询所有的地铁线路票价以及首末车信息，为您的出行提供帮助（本次更新增加了16号线内容，抓取并更新了部分线路的首末车时间）。",
     welcome: "欢迎使用北京地铁票价助手，by Henry.Ma",
-    ballleft: -20,
+
+    oriLeft: 0,
+    oriTop: 0,
+
+    ballleft: 0,
     screenWidth: 0,
-    balltop: -20,
+    balltop: 0,
+
+    startX: 0,
+    startY: 0,
   },
 
   onLoad: function () {
@@ -32,7 +39,15 @@ Page({
     console.log('屏幕宽度：' + this.data.screenWidth)
     console.log('启始pageX: ' + pageX);
 
-    //这里用right和bottom.所以需要将pageX pageY转换  
+    var newLeft = this.data.oriLeft;
+
+    if (this.data.startX > pageX) { // move to left
+      newLeft -= (this.data.startX - pageX);
+    } else {
+      newLeft += (pageX - this.data.startX);
+    }
+
+    /*这里用right和bottom.所以需要将pageX pageY转换  
     var x = this.data.screenWidth / 2 - pageX - 20;
     if (this.data.screenWidth > 385) {
       if (x > 42) { x = 42; }
@@ -40,10 +55,24 @@ Page({
       if (x > 32) { x = 32; }
     }
     if (x < 0) { x = 0; }
-    console.log('x:' + x)
+    */
+
+    console.log('NEW LEFT:' + newLeft)
     this.setData({
-      ballleft: -x,
+      ballleft: newLeft,
       balltop: 20,
     });
+  },
+
+  ballMoveEndEvent: function (e) {
+    //this.data.oriLeft = e.currentTarget.offsetLeft;
+    //this.data.oriTop = e.currentTarget.offsetTop;
+  },
+
+  ballMoveStartEvent: function (e) {
+    this.data.startX = e.touches[0].pageX;
+    this.data.startY = e.touches[0].pageY;
+    this.data.oriLeft = e.currentTarget.offsetLeft;
+    this.data.oriTop = e.currentTarget.offsetTop;
   },
 })
